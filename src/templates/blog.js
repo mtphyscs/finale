2d5dd5b5d83f51b99ac777dc0af4f658cbd1c1ec
“jpoveda22” <jpoveda226@gmail.com>
2020-04-12 17:56:43 -0600

    blog template path

---
+++ src/templates/blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const Blog = () => {
+    return (
+        <Layout>
+            Blog template
+        </Layout>
+
+    )
+}
+
+export default blog
