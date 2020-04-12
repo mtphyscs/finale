0d2b5d5e28b9fcd883acba12d9c41457baeeb807
“jpoveda22” <jpoveda226@gmail.com>
2020-04-12 12:57:07 -0600

    sass plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,13 +2,16 @@ import React from "react"
 
 import Header from './header'
 import Footer from './footer'
-import '../styles/index.css'
+import '../styles/index.scss'
+import layoutStyles from './layout.module.scss'
 
 const Layout = (props) => {
   return (
-    <div>
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
       <Header />
       {props.children}
+      </div>
       <Footer />
     </div>
   )
