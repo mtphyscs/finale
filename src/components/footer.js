f003b02d0b71540e8152ca4a8d63a09721e4bc23
“jpoveda22” <jpoveda226@gmail.com>
2020-04-12 22:05:16 -0600

    sass

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useStaticQuery, graphql} from 'gatsby'
+import footerStyles from './footer.module.scss'
 
 const Footer = () => {
     const data = useStaticQuery(graphql`
@@ -14,7 +15,8 @@ const Footer = () => {
 
 
     return (
-        <footer>
+        <footer className={footerStyles.footer}>
+          <hr/>
             <p>Written, designed, and built by {data.site.siteMetadata.author}. © 2020</p>
         </footer>
     )
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import blogStyles from './blog.module.scss'
 
 
 import Layout from "../components/layout"
@@ -30,10 +31,10 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-     <ol>
+     <ol className={blogStyles.posts}>
        {data.allMarkdownRemark.edges.map((edge)=>{
          return(
-           <li>
+           <li className={blogStyles.post}>
              <Link to={`/blog/${edge.node.fields.slug}`}>
              <h2>{edge.node.frontmatter.title}</h2> 
              <p>{edge.node.frontmatter.date}</p>
