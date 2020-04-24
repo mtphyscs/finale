26e14720ca095d5fc53c5878e5dc710037c6d49f
“jpoveda22” <jpoveda226@gmail.com>
2020-04-23 23:25:16 -0600

    done

--- src/pages/library.js
+++ src/pages/library.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import Head from '../components/head'
+import libraryStyles from './library.module.scss'
 
 const Library = () => {
   const data = useStaticQuery(graphql`
@@ -23,19 +24,22 @@ const Library = () => {
   `)
 
 
+
  
   return (
     <Layout>
       <Head title="Library" />
       <h1>Library</h1>
+      <h2>Here you’ll find an occasionally-updated list of books I’ve read and enjoyed.</h2>
       <ol>
        {data.allAmazonWishlistItem.edges.map((edge)=>{
          return(
            <li>
              <Link to={`/book/${edge.node.title}`}>
-             <h2>{edge.node.title}</h2> 
-             <p>{edge.node.comment}</p> 
-             <img src={edge.node.image.url}></img>
+             <img src={edge.node.image.url.replace("SS135_.jpg", "LL135_.jpg")}></img>
+             <h2 className={libraryStyles.a}>{edge.node.title}</h2> 
+          
+             
             
             
              </Link>
