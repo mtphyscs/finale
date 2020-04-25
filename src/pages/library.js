9659584752917ae08193752e945ed64f5521e123
“jpoveda22” <jpoveda226@gmail.com>
2020-04-25 17:23:50 -0600

    failed

--- src/pages/library.js
+++ src/pages/library.js
@@ -1,8 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import Head from '../components/head'
-import libraryStyles from './library.module.scss'
+import { Link, useStaticQuery, graphql } from "gatsby"
+import Head from "../components/head"
+import libraryStyles from "./library.module.scss"
 
 const Library = () => {
   const data = useStaticQuery(graphql`
@@ -23,32 +23,26 @@ const Library = () => {
     }
   `)
 
-
-
- 
   return (
     <Layout>
       <Head title="Library" />
       <h1>Library</h1>
-      <h2>Here you’ll find an occasionally-updated list of books I’ve read and enjoyed.</h2>
+      <h2>
+        Here you’ll find an occasionally-updated list of books I’ve read and
+        enjoyed.
+      </h2>
       <ol>
-       {data.allAmazonWishlistItem.edges.map((edge)=>{
-         return(
-           <li>
-             <Link to={`/book/${edge.node.title}`}>
-             <img src={edge.node.image.url.replace("SS135_.jpg", "LL135_.jpg")}></img>
-             <h2 className={libraryStyles.a}>{edge.node.title}</h2> 
-          
-             
-            
-            
-             </Link>
-           </li>
-         )
-
-       })}
-     </ol>
-      
+        {data.allAmazonWishlistItem.edges.map(edge => {
+          return (
+            <li>
+              <Link to={`/book/${edge.node.title}`}>
+                {/* <img src={edge.node.image.url.replace("SS135_.jpg", "LL135_.jpg")} alt=""></img> */}
+                <h2 className={libraryStyles.a}>{edge.node.title}</h2>
+              </Link>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
