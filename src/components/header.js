02e2578694ae607d6aca8e68687220e0e9930d16
形而上学 <47684055+mtphyscs@users.noreply.github.com>
2020-05-18 11:31:06 -0600

    update

--- src/components/header.js
+++ src/components/header.js
@@ -25,6 +25,7 @@ const Header = () => {
       <nav className={headerStyles.nav}>
         <ul className={headerStyles.navList}>
           <li>
+            
             <Link
               className={headerStyles.navItem}
               activeClassName={headerStyles.activeNavItem}
@@ -73,14 +74,13 @@ const Header = () => {
             <Link
               className={headerStyles.navItem}
               activeClassName={headerStyles.activeNavItem}
-              to="/visual-art"
+              to="/contact"
             >
-              Visual Art
+              Contact
             </Link>
           </li>
         </ul>
       </nav>
-      {/* <hr width="100%"/> */}
     </header>
   
   )
